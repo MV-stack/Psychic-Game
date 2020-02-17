c0b7734411f5e0c3b25f9d0b501061673571aeab
MV-stack <59325046+MV-stack@users.noreply.github.com>
2020-02-16 21:40:37 -0700

    updates to html and game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var losses = 0;
+var guessedLetters = [];
+var letterToGuess = null;
+var totalGuesses = 10;
+
+function randomLetterGen() {
+    letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+    console.log(letterToGuess);
+}
